23fc485688ca1d7dd83f8fa619c134859b41b2b6
atuffa <tuffajr@gmail.com>
2018-05-24 13:45:24 -0500

    Database Created

--- config/connection.js
+++ config/connection.js
@@ -1,3 +1,18 @@
 var mysql = require("mysql"); 
 
-// abdisa will be adding code her for the database connection
+let connection;
+if(process.env.JAWSDB_URL) {
+  //Heroku deployment
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+  //local host
+    connection = mysql.createConnection({
+        root: 3000,
+        host: "localhost",
+        user: "root",
+        password: "",
+        database: "",
+    });
+};
+
+// project-two-nfl.herokuapp.com
