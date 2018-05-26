e5964483df67a0a1971ac3c5a4686851a271a4ba
Koch <kochm@vikings.nfl.net>
2018-05-26 13:19:21 -0500

    populate more users in NFL_DB.users table

---
+++ db/seeds-users.sql
@@ -0,0 +1,20 @@
+INSERT INTO users (username, points)
+VALUES ("Abdisa", 10);
+INSERT INTO users (username, points)
+VALUES ("Solomon", 17);
+INSERT INTO users (username, points)
+VALUES ("Madison", 16);
+INSERT INTO users (username, points)
+VALUES ("Robert", 9);
+INSERT INTO users (username, points)
+VALUES ("Cavan", 9);
+INSERT INTO users (username, points)
+VALUES ("Gregg",18);
+INSERT INTO users (username, points)
+VALUES ("Crocodile23", 93);
+INSERT INTO users (username, points)
+VALUES ("VikesFan", 157);
+INSERT INTO users (username, points)
+VALUES ("PackersSuck", 77);
+INSERT INTO users (username, points)
+VALUES ("SeeHawksLose", 83);
