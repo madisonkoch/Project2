9add5b9639c7cb0bc38b9123527b63c1057a09ed
atuffa <tuffajr@gmail.com>
2018-05-26 11:19:59 -0500

    creating framework routes

--- config/connection.js
+++ config/connection.js
@@ -1,5 +1,8 @@
 var mysql = require("mysql"); 
 
+//require th env password
+require("dotenv").config();
+
 // abdisa will be adding code here for the database connection(s)
 let connection;
 if(process.env.JAWSDB_URL) {
@@ -8,11 +11,11 @@ if(process.env.JAWSDB_URL) {
 } else {
   //local host
     connection = mysql.createConnection({
-        root: 3000,
+        port: 3306,
         host: "localhost",
         user: "root",
-        password: "",
-        database: "",
+        password: "Koji@191991",
+        database: "z38xzhv5bfjbkmeg",
     });
 };
 
--- controllers/nfl.js
+++ controllers/nfl.js
@@ -1,6 +1,18 @@
-
+// Dependencies Required 
 const express = require("express");
-
 const router = express.Router();
 
-// initial api calls here 
+// Import model to use its databse functions
+// let nfl = (../models/nfl.js)
+
+router.get("/", function(req, res){
+  // response
+});
+
+router.post("/api/teams", function(req, res){
+    //response
+})
+
+router.put("/api/teams/:id", function(req, res){
+    //response
+})
