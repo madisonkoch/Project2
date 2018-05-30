f599d86cea3b9795e8457221c20fee929eba439b
Cavan Wagg <waggx002@gmail.com>
2018-05-29 21:30:12 -0500

    sequelize find all users, leaderboard top 5

--- controllers/nfl.js
+++ controllers/nfl.js
@@ -4,17 +4,44 @@
 var path = require("path");
 // Import model to use its databse functions
 var db = require("../models");
+var Sequelize = require("Sequelize");
 
 module.exports = function(app) {
 
 
-
+// * Get all teams
     app.get("/", function(req, res){
-        db.teams.findAll({})
+        db.teams.findAll({
+            attributes: ['team_name']
+        })
         .then(function(dbNfl) {  
             res.json(dbNfl);
         });
     });
+// * Get all user data
+    app.get("/user", function(req, res){
+        db.user.findAll({})
+        .then(function(dbUser) {  
+            res.json(dbUser);
+        });
+    });
+
+
+    // * Get top 5 scores 
+    app.get("/leaderboard", function(req, res){
+        // a leraderboard of all the high scores
+        db.user.findAll({ limit: 5,
+           order: [
+               ['points', 'DESC'],
+        ],      
+        })
+        .then(function(dbUser) {  
+            res.json(dbUser);
+
+
+    });
+});
+
     
     // app.post("/api/teams", function(req, res){
     //     //response
@@ -33,13 +60,11 @@ module.exports = function(app) {
     // })
     
     // app.get("/profile", function(req, res){
-    //     // generated user profiles after logging in
-    //     // will have some statistics
+        // generated user profiles after logging in
+        // will have some statistics
     // })
     
-    // app.get("/leaderboard", function(req, res){
-    //     // a leraderboard of all the high scores
-    // })
+    
 
 };
 
--- models/player.js
+++ models/player.js
@@ -1,11 +1,12 @@
 // Import the ORM to create functions that will interact with the database.
 // var orm = require(""../config/orm.js");
+var Sequelize = require("Sequelize");
+
 module.exports = function(sequelize, DataTypes) {
   var user = sequelize.define("user", {
-    username: DataTypes.STRING,
+    username: {type: Sequelize.STRING, primaryKey: true},
     points: DataTypes.INTEGER
   }, {
-    id: false,
     timestamps: false
   });
   return user;
