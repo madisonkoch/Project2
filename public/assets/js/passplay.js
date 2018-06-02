83b48034b2ec06ef5d11d06e4b69ded4a9c2bca6
scoslo5512 <robert.queeney@gmail.com>
2018-06-02 08:26:47 -0700

    Pass and run working, Master file (testies) not able to link b/c of 'require'

--- public/assets/js/passplay.js
+++ public/assets/js/passplay.js
@@ -1,84 +1,90 @@
-const offPass = 7.8
-const defPass = 5.5
-let startingYardage = 20
+let passYardage = 0
 
-var toString = Object.prototype.toString,
-slice = Array.prototype.slice;
-function Probability() {
-    var i = 0,
-        l = 0,
-        probas = [],
-        functions = [],
-        sum = 0,
-        args = toString.call(arguments[0]) === '[object Array]' ? arguments[0] : slice.call(arguments);
-    args.push({
-        p: 0,
-        f: function () {}
-    });
-    for (i = 0, l = args.length; i < l; i++) {
-        var p = Math.abs(parseFloat(args[i].p)),
-            f = args[i].f;
-        if (isNaN(p) || typeof f !== 'function') {
-            throw new TypeError('Probability.js: Invalid probability object in argument ' + i + '.');
+function passPlay() {
+    var toString = Object.prototype.toString,
+        slice = Array.prototype.slice;
+    function Probability() {
+        var i = 0,
+            l = 0,
+            probas = [],
+            functions = [],
+            sum = 0,
+            args = toString.call(arguments[0]) === '[object Array]' ? arguments[0] : slice.call(arguments);
+        args.push({
+            p: 0,
+            f: function () { }
+        });
+        for (i = 0, l = args.length; i < l; i++) {
+            var p = Math.abs(parseFloat(args[i].p)),
+                f = args[i].f;
+            if (isNaN(p) || typeof f !== 'function') {
+                throw new TypeError('Probability.js: Invalid probability object in argument ' + i + '.');
+            }
+            if (/%/.test(args[i].p)) {
+                p = p / 100.0;
+            }
+            sum += p;
+            if (sum > 1.0) {
+                throw new TypeError('Probability.js: Probability exceeds "1.0" (=100%) in argument ' + i + ': p="' + p + '" (=' + p * 100 + '%), sum="' + sum + '" (=' + sum * 100 + '%).');
+            }
+            probas[i] = sum;
+            functions[i] = f;
         }
-        if (/%/.test(args[i].p)) {
-            p = p / 100.0;
-        }
-        sum += p;
-        if (sum > 1.0) {
-            throw new TypeError('Probability.js: Probability exceeds "1.0" (=100%) in argument ' + i + ': p="' + p + '" (=' +  p * 100 + '%), sum="' + sum + '" (=' +  sum * 100 + '%).');
-        }
-        probas[i] = sum;
-        functions[i] = f;
+        return function probabilitilized() {
+            var random = Math.random();
+            for (i = 0, l = probas.length - 1; i < l && random >= probas[i]; i++) {
+                /* intentionally left empty */
+            }
+            return functions[i].apply(this, arguments);
+        };
     }
-    return function probabilitilized() {
-        var random = Math.random();
-        for (i = 0, l = probas.length - 1; i < l && random >= probas[i]; i++) {
-            /* intentionally left empty */
-        }
-        return functions[i].apply(this, arguments);
-    };
+
+
+    function getRandomInt(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function incompletePass() {
+        console.log("The pass was incomplete")
+    }
+
+    function bigDefPlay() {
+        let yards = getRandomInt(8, 1)
+        let roundedYards = Math.round(yards * 10) / 10;
+        console.log(`Oooohh.  The Vikings got blown up by a sack for a loss of ${roundedYards} yards!`)
+        passYardage -= roundedYards
+        console.log(passYardage)
+        return passYardage
+    }
+
+    function normalPass() {
+        let yards = getRandomInt(offPass, defPass)
+        let roundedYards = Math.round(yards * 10) / 10;
+        console.log(`Pass complete for a gain of ${roundedYards} yards!`)
+        passYardage += roundedYards
+        console.log(passYardage)
+        return passYardage
+    }
+
+    function bigPassPlay() {
+        let yards = getRandomInt(80, 20)
+        let roundedYards = Math.round(yards * 10) / 10;
+        console.log(`Holy fucking shit!  The Vikings strike for ${roundedYards} yards!`)
+        passYardage += roundedYards
+        console.log(passYardage)
+        return passYardage
+    }
+
+    var probabilitilized = new Probability({ p: "30%", f: incompletePass }, { p: "13%", f: bigDefPlay }, { p: "52%", f: normalPass }, { p: "5%", f: bigPassPlay })
+
+    for (let i = 0; i < 1; i++) {
+        probabilitilized();
+    }
 }
 
+module.exports = passPlay;
+module.exports = { passYardage }
 
-function getRandomInt(min, max) {
-    return Math.random() * (max - min) + min; 
-}
 
-function incompletePass(){
-    console.log("The pass was incomplete")
-}
 
-function bigDefPlay (){
-    let yards = getRandomInt(8, 1)
-    let roundedYards = Math.round( yards * 10 ) / 10;
-    console.log(`Oooohh.  The Vikings got blown up by a sack for a loss of ${roundedYards} yards!`)
-    startingYardage = startingYardage - roundedYards
-    console.log(startingYardage)
-    return startingYardage
-}
-
-function normalPass (){
-    let yards = getRandomInt(offPass, defPass)
-    let roundedYards = Math.round( yards * 10 ) / 10;
-    console.log(`Pass complete for a gain of ${roundedYards} yards!`)
-    startingYardage = startingYardage + roundedYards
-    console.log(startingYardage)
-    return startingYardage
-}
-
-function bigPassPlay (){
-    let yards = getRandomInt(80, 20)
-    let roundedYards = Math.round( yards * 10 ) / 10;
-    console.log(`Holy fucking shit!  The Vikings strike for ${roundedYards} yards!`)
-    startingYardage = startingYardage + roundedYards
-    console.log(startingYardage)
-    return startingYardage
-}
-
-var probabilitilized = new Probability({p: "30%", f: incompletePass}, {p:"13%", f: bigDefPlay}, {p: "52%", f: normalPass}, {p: "5%", f:bigPassPlay})
-
-for (let i = 0; i < 1; i++){
-    probabilitilized(); 
-  }
 
--- public/assets/js/play.js
+++ public/assets/js/play.js
@@ -1,89 +1,96 @@
+let runYardage = 0;
 
-const offRun = 6.2
-const defRun = 3.4
-let startingYardage = 20
-
-var toString = Object.prototype.toString,
-slice = Array.prototype.slice;
-function Probability() {
-    var i = 0,
-        l = 0,
-        probas = [],
-        functions = [],
-        sum = 0,
-        args = toString.call(arguments[0]) === '[object Array]' ? arguments[0] : slice.call(arguments);
-    args.push({
-        p: 0,
-        f: function () {}
-    });
-    for (i = 0, l = args.length; i < l; i++) {
-        var p = Math.abs(parseFloat(args[i].p)),
-            f = args[i].f;
-        if (isNaN(p) || typeof f !== 'function') {
-            throw new TypeError('Probability.js: Invalid probability object in argument ' + i + '.');
+function runPlay() {
+    var toString = Object.prototype.toString,
+        slice = Array.prototype.slice;
+    function Probability() {
+        var i = 0,
+            l = 0,
+            probas = [],
+            functions = [],
+            sum = 0,
+            args = toString.call(arguments[0]) === '[object Array]' ? arguments[0] : slice.call(arguments);
+        args.push({
+            p: 0,
+            f: function () { }
+        });
+        for (i = 0, l = args.length; i < l; i++) {
+            var p = Math.abs(parseFloat(args[i].p)),
+                f = args[i].f;
+            if (isNaN(p) || typeof f !== 'function') {
+                throw new TypeError('Probability.js: Invalid probability object in argument ' + i + '.');
+            }
+            if (/%/.test(args[i].p)) {
+                p = p / 100.0;
+            }
+            sum += p;
+            if (sum > 1.0) {
+                throw new TypeError('Probability.js: Probability exceeds "1.0" (=100%) in argument ' + i + ': p="' + p + '" (=' + p * 100 + '%), sum="' + sum + '" (=' + sum * 100 + '%).');
+            }
+            probas[i] = sum;
+            functions[i] = f;
         }
-        if (/%/.test(args[i].p)) {
-            p = p / 100.0;
-        }
-        sum += p;
-        if (sum > 1.0) {
-            throw new TypeError('Probability.js: Probability exceeds "1.0" (=100%) in argument ' + i + ': p="' + p + '" (=' +  p * 100 + '%), sum="' + sum + '" (=' +  sum * 100 + '%).');
-        }
-        probas[i] = sum;
-        functions[i] = f;
+        return function probabilitilized() {
+            var random = Math.random();
+            for (i = 0, l = probas.length - 1; i < l && random >= probas[i]; i++) {
+                /* intentionally left empty */
+            }
+            return functions[i].apply(this, arguments);
+        };
     }
-    return function probabilitilized() {
-        var random = Math.random();
-        for (i = 0, l = probas.length - 1; i < l && random >= probas[i]; i++) {
-            /* intentionally left empty */
-        }
-        return functions[i].apply(this, arguments);
-    };
-}
+
+
 
     function getRandomInt(min, max) {
-        min = Math.ceil(min);
-        max = Math.floor(max);
-        return Math.floor(Math.random() * (max - min)) + min; 
+        return Math.random() * (max - min) + min;
     }
 
     function bigRunPlay() {
         let yards = getRandomInt(80, 20)
-        let roundedYards = Math.round( yards * 10 ) / 10;
+        let roundedYards = Math.round(yards * 10) / 10;
         console.log(`Holy cow!  The vikings strike for a ${roundedYards} yard play!`)
-        startingYardage = startingYardage + roundedYards
-        console.log(startingYardage)
-        return startingYardage
+        runYardage += roundedYards
+        console.log(runYardage)
+        return runYardage
     }
 
-    function bigDefPlay (){
+    function bigDefPlay() {
         let yards = getRandomInt(8, 1)
-        let roundedYards = Math.round( yards * 10 ) / 10;
+        let roundedYards = Math.round(yards * 10) / 10;
         console.log(`Holy smokes!  The defense just stuffed the vikings for a loss of ${roundedYards} yards!`)
-        startingYardage = startingYardage - roundedYards
-        console.log(startingYardage)
-        return startingYardage
+        runYardage -= roundedYards
+        console.log(runYardage)
+        return runYardage
     }
-    
 
-    function normalRun(){
+
+    function normalRun() {
         let yards = getRandomInt(offRun, defRun)
-        let roundedYards = Math.round( yards * 10 ) / 10;
+        let roundedYards = Math.round(yards * 10) / 10;
         console.log(`The Vikings pound the ball for a gain of ${roundedYards} yards!`)
-        startingYardage = startingYardage + roundedYards
-        console.log(startingYardage)
-        return startingYardage
+        runYardage += roundedYards
+        console.log(runYardage)
+        return runYardage
     }
 
-    function noGain(){
+    function noGain() {
         console.log("The defense comes up big and stuffs the runner for no gain")
     }
 
-    var probabilitilized = new Probability({p: "8%", f: bigRunPlay}, {p:"8%", f: bigDefPlay}, {p: "60%", f: normalRun}, {p: "14%", f:noGain})
+    var probabilitilized = new Probability({ p: "8%", f: bigRunPlay }, { p: "8%", f: bigDefPlay }, { p: "60%", f: normalRun }, { p: "14%", f: noGain })
 
-    for (let i = 0; i < 1; i++){
-        probabilitilized(); 
+    for (let i = 0; i < 1; i++) {
+        probabilitilized();
+
     }
 
-    // console.log(prob)
+}
+
+module.exports = runPlay;
+module.exports = { runYardage }
+
+
+
+
+
 
