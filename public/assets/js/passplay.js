2097b0bc2c2afdadc3bafa48380bc87d5bcc2bbd
scoslo5512 <robert.queeney@gmail.com>
2018-06-02 11:26:29 -0700

    Limited to one file.  Got it all working (think so)

--- public/assets/js/passplay.js
+++ public/assets/js/passplay.js
@@ -1,5 +1,17 @@
 let passYardage = 0
+let runYardage = 0
+const offRun = 6.2
+const defRun = 3.4
+const offPass = 8.5
+const defPass = 4.6
+// totalYardage used to calculate field position and yardage to score
+let totalYardage = 20
+// current yardage used to calculate down vs distance 
+let currentYardage = 0
+let totalPoints = 0
+let downs = 1
 
+$( document ).ready(function(){
 function passPlay() {
     var toString = Object.prototype.toString,
         slice = Array.prototype.slice;
@@ -46,6 +58,8 @@ function passPlay() {
 
     function incompletePass() {
         console.log("The pass was incomplete")
+        passYardage = 0
+        return passYardage
     }
 
     function bigDefPlay() {
@@ -82,9 +96,146 @@ function passPlay() {
     }
 }
 
-module.exports = passPlay;
-module.exports = { passYardage }
 
 
+function runPlay() {
+    var toString = Object.prototype.toString,
+        slice = Array.prototype.slice;
+    function Probability() {
+        var i = 0,
+            l = 0,
+            probas = [],
+            functions = [],
+            sum = 0,
+            args = toString.call(arguments[0]) === '[object Array]' ? arguments[0] : slice.call(arguments);
+        args.push({
+            p: 0,
+            f: function () { }
+        });
+        for (i = 0, l = args.length; i < l; i++) {
+            var p = Math.abs(parseFloat(args[i].p)),
+                f = args[i].f;
+            if (isNaN(p) || typeof f !== 'function') {
+                throw new TypeError('Probability.js: Invalid probability object in argument ' + i + '.');
+            }
+            if (/%/.test(args[i].p)) {
+                p = p / 100.0;
+            }
+            sum += p;
+            if (sum > 1.0) {
+                throw new TypeError('Probability.js: Probability exceeds "1.0" (=100%) in argument ' + i + ': p="' + p + '" (=' + p * 100 + '%), sum="' + sum + '" (=' + sum * 100 + '%).');
+            }
+            probas[i] = sum;
+            functions[i] = f;
+        }
+        return function probabilitilized() {
+            var random = Math.random();
+            for (i = 0, l = probas.length - 1; i < l && random >= probas[i]; i++) {
+                /* intentionally left empty */
+            }
+            return functions[i].apply(this, arguments);
+        };
+    }
+    function getRandomInt(min, max) {
+        return Math.random() * (max - min) + min;
+    }
+
+    function bigRunPlay() {
+        let yards = getRandomInt(80, 20)
+        let roundedYards = Math.round(yards * 10) / 10;
+        console.log(`Holy cow!  The vikings strike for a ${roundedYards} yard play!`)
+        runYardage += roundedYards
+        console.log(runYardage)
+        return runYardage
+    }
+
+    function bigDefPlay() {
+        let yards = getRandomInt(8, 1)
+        let roundedYards = Math.round(yards * 10) / 10;
+        console.log(`Holy smokes!  The defense just stuffed the vikings for a loss of ${roundedYards} yards!`)
+        runYardage -= roundedYards
+        console.log(runYardage)
+        return runYardage
+    }
+
+
+    function normalRun() {
+        let yards = getRandomInt(offRun, defRun)
+        let roundedYards = Math.round(yards * 10) / 10;
+        console.log(`The Vikings pound the ball for a gain of ${roundedYards} yards!`)
+        runYardage += roundedYards
+        console.log(runYardage)
+        return runYardage
+    }
+
+    function noGain() {
+        console.log("The defense comes up big and stuffs the runner for no gain")
+    }
+
+    var probabilitilized = new Probability({ p: "8%", f: bigRunPlay }, { p: "8%", f: bigDefPlay }, { p: "60%", f: normalRun }, { p: "14%", f: noGain })
+
+    for (let i = 0; i < 1; i++) {
+        probabilitilized();
+
+    }
+}
+
+function totalYards (){
+    if (totalYardage >= 80) {
+        totalPoints + 7; 
+        document.getElementById("score").innerHTML=totalPoints
+}}
+
+function firstDown (){
+    if (currentYardage >= 10) {
+        console.log("First Down!"); 
+        downs = 1
+        currentYardage = 0
+    } else {
+        downs += 1
+        console.log("no first")
+    }
+}
+
+function totalDowns(){
+    if (downs > 4 && currentYardage < 10 ) {
+        console.log("turnover")
+        totalYardage = 20
+        downs = 1
+        currentYardage = 0
+    }
+}
+
+$("#big_butt").on("click", function() {
+    passPlay(); 
+    totalYardage += passYardage
+    currentYardage += passYardage
+    passYardage = 0
+    totalYards();
+    firstDown(); 
+    totalDowns(); 
+    document.getElementById("downs").innerHTML=downs
+    document.getElementById("yardage").innerHTML=currentYardage
+    document.getElementById("data_place").innerHTML=totalYardage
+    document.getElementById("score").innerHTML=totalPoints
+    console.log("current yards", currentYardage)
+    
+  });
+
+  $("#butt").on("click", function() {
+    runPlay();
+    totalYardage += runYardage
+    currentYardage += runYardage
+    runYardage = 0
+    totalYards();
+    firstDown(); 
+    totalDowns(); 
+    document.getElementById("downs").innerHTML=downs
+    document.getElementById("yardage").innerHTML=currentYardage
+    document.getElementById("data_place").innerHTML=totalYardage
+    document.getElementById("score").innerHTML=totalPoints
+    console.log("current yards", currentYardage)
+  });
+}); 
 
 
