65d5568db2db7cbdde1d2cd43ca4dba135ec852f
Koch <kochm@vikings.nfl.net>
2018-06-05 10:40:58 -0500

    fixed layering on game.handlebars

--- public/assets/js/nongame.js
+++ public/assets/js/nongame.js
@@ -1,21 +1,45 @@
-// Save selected opponent's stats in local storage
-    $('#dropdown').change(function() {
-        var opponentValues = this.value;
-    localStorage.setItem('opponentValues', JSON.stringify(opponentValues));
-    // append to different screen
-    });
+//Game Page
+    // Save selected opponent's stats in local storage
+        $('#dropdown').change(function() {
+            var opponentValues = this.value;
+        localStorage.setItem('opponentValues', JSON.stringify(opponentValues));
+        // append to different screen
+        });
 
 
-//Close pre-game modal after
-    var modal = document.getElementById('pre-game-modal');
+    //Close pre-game modal after
+        var modal = document.getElementById('pre-game-modal');
 
-    // When the user clicks anywhere outside of the modal, close it
-    $('#close-btn').onclick = function(event) {
-        if (event.target == modal) {
-            modal.style.display = "none";
+        // When the user clicks anywhere outside of the modal, close it
+        $('#close-btn').onclick = function(event) {
+            if (event.target == modal) {
+                modal.style.display = "none";
+            }
         }
-    }
+    //Ball & Player Image
+        //Make div around image the entire height of screen
+            $(function(){
+                var windowH = $(window).height();
+                var divH = $('#lineup-div').height();
+                if(windowH > divH) {                            
+                    $('#lineup-div').css({'height':($(window).height())+'px'});
+                }                                                                               
+                $(window).resize(function(){
+                    var windowH = $(window).height();
+                    var divH = $('#lineup-div').height();
+                    var differenceH = windowH - divH;
+                    var newH = divH + differenceH;
+                    var truecontentH = $('#truecontent').height();
+                    if(windowH > truecontentH) {
+                        $('#lineup-div').css('height', (newH)+'px');
+                    }
+            
+                })          
+            });
 
+        // let factor = (1558/1242);
+        // var y = $("body").width * factor;
+        // $("#lineup").css({bottom:y});
 
 //About Page
     //Show/hide Instructions
